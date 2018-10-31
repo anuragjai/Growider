eff02b9baec3edc21b1e250fa03fb07dbb8dc9d0
anuragjai <14mcmc08@gmail.com>
2018-10-31 12:45:31 +0530

    final

--- handler.php
+++ handler.php
@@ -21,6 +21,6 @@ $validator->field('message')->maxLength(6000);
 
 
 
-$pp->sendEmailTo('14mcmc08@gmail.com'); // ← Your email here
+$pp->sendEmailTo('alok@growider.in'); // ← Your email here
 
 echo $pp->process($_POST);
